72f75e485c0f35354532c1b25ea58d404ff9de9d
aasli <aasli@ASLINB>
2023-09-02 01:07:31 +0300

    Proje dosyası ekle.

---
+++ SecondTry/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int sayi = 2;
+        Console.WriteLine(sayi);
+        Console.WriteLine(sayi + 2);
+        Console.WriteLine(sayi * sayi);
+        Console.WriteLine(sayi - 5);
+
+        Console.ReadKey();
+    }
+}
